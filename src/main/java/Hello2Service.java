4db81ecbacfafbb5be7d73dad9faf612176c683a
Asgeir Frimannsson <asgeirf@localhost.localdomain>
2010-05-08 19:11:49 +1000

    modified to run on tomcat

--- src/main/java/Hello2Service.java
+++ src/main/java/Hello2Service.java
@@ -1,26 +1,16 @@
 import javax.enterprise.context.RequestScoped;
 import javax.inject.Inject;
-import javax.inject.Named;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 
-@Path("/hello")
-public class HelloService {
+public class Hello2Service {
 
-	@Inject Hello2Service hello2Service;
-	
 	@GET
 	@Produces("text/html")
+	@Path("")
 	public String sayHello(){
-		return "hello";
+		return "hello 2";
 	}
-	
-	@Path("/2")
-	public Hello2Service hello2service(){
-		return hello2Service;
-	}
-	
-
 	
 }
--- src/main/java/MyApplication.java
+++ src/main/java/MyApplication.java
@@ -1,18 +1,14 @@
-
-
 import java.util.HashSet;
 
 import java.util.Set;
 
 import javax.ws.rs.core.Application;
 
-public class MyApplication extends Application
-{
-   @Override
-   public Set<Class<?>> getClasses()
-   {
-      Set<Class<?>> classes = new HashSet<Class<?>>();
-      classes.add(HelloService.class);
-      return classes;
-   }
+public class MyApplication extends Application {
+	@Override
+	public Set<Class<?>> getClasses() {
+		Set<Class<?>> classes = new HashSet<Class<?>>();
+		classes.add(HelloService.class);
+		return classes;
+	}
 }
